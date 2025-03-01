cc667253c7eedabb3fb68d3dbc68934b20d66ce3
Zufishan14 <mohammedzufishan@gmail.com>
2025-03-02 01:08:43 +0530

    9th Commit

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -6,3 +6,9 @@ class SignUpForm(UserCreationForm):
     class Meta: 
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Username', 'class': 'w-full py-4 px-6 rounded'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Enter Email', 'class': 'w-full py-4 px-6 rounded'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'w-full py-4 px-6 rounded'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password', 'class': 'w-full py-4 px-6 rounded'}))  
+    
