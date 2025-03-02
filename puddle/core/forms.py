be8c901cd68ba9b99849788f2a56e87a80d54541
Zufishan14 <mohammedzufishan@gmail.com>
2025-03-02 19:27:20 +0530

    10th Commit

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -1,6 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm): 
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Username', 'class': 'w-full py-4 px-6 rounded'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'w-full py-4 px-6 rounded'}))
 
 class SignUpForm(UserCreationForm):
     class Meta: 
@@ -11,4 +15,3 @@ class SignUpForm(UserCreationForm):
     email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Enter Email', 'class': 'w-full py-4 px-6 rounded'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'w-full py-4 px-6 rounded'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password', 'class': 'w-full py-4 px-6 rounded'}))  
-    
